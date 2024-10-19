35ca0fde3adf68bd6398bf9028ab05191567cf42
Thusan Arul <piruthusan@gmail.com>
2024-10-19 02:53:41 +0200

    Creates func that parses html and gets links to visit for a page - This commit also removes reqeasyhtml. instead floki is used directly

--- lib/sculptures_in_oslo.ex
+++ lib/sculptures_in_oslo.ex
@@ -1,27 +1,39 @@
 defmodule SculpturesInOslo do
+  @baseUrl "https://okk.kunstsamlingen.no"
   @moduledoc """
   Documentation for `SculpturesInOslo`.
   """
 
   @doc """
-  Hello world.
+  Get links to visit from main page
 
   ## Examples
 
-      iex> SculpturesInOslo.hello()
-      :world
+      iex> SculpturesInOslo.get_links_from_page(1)
+      [{:title, ["a sculpture title"], :link, "/a-relative-link" }]
 
   """
-  def hello do
-    req = Req.new(http_errors: :raise) |> ReqEasyHTML.attach()
+  def get_links_from_page(pagenr) do
+    req = Req.new(http_errors: :raise)
 
     body =
       Req.get!(req,
-        url:
-          "https://okk.kunstsamlingen.no/objects/images?filter=classification%3ASkulptur&sort=Relevance#filters"
+        url: main_page(pagenr)
       ).body
 
-    IO.puts(body)
-    :world
+    {:ok, document} = Floki.parse_document(body)
+
+    links_to_visit =
+      document
+      |> Floki.find("div.title a.detailLink")
+      |> Enum.map(fn x ->
+        {:title, Floki.attribute(x, "a", "title"), :link, Floki.attribute(x, "a", "href")}
+      end)
+
+    links_to_visit
+  end
+
+  defp main_page(pagenr) do
+    "#{@baseUrl}/objects/images?filter=classification%3ASkulptur&page=#{pagenr}&sort=Relevance#filters"
   end
 end
--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule SculpturesInOslo.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:req, "~> 0.5.0"},
-      {:req_easyhtml, "~> 0.1.0"}
+      {:floki, "~> 0.36.0"}
     ]
   end
 end
