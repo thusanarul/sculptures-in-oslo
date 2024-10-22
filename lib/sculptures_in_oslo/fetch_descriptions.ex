34b82824a6da67e4d83f0b4363d680672fd4502e
Thusan Arul <piruthusan@gmail.com>
2024-10-22 21:40:26 +0200

    wip: start FetchDescriptions after FetchLinks
    this does not work at all

--- lib/sculptures_in_oslo/fetch_descriptions.ex
+++ lib/sculptures_in_oslo/fetch_descriptions.ex
@@ -1,3 +1,36 @@
 defmodule SculpturesInOslo.FetchDescriptions do
+  alias SculpturesInOslo.LinksToVisit
   use Task
+  @baseUrl "https://okk.kunstsamlingen.no"
+
+  def start_link(arg) do
+    Task.start_link(__MODULE__, :run, [arg])
+  end
+
+  def run do
+    IO.puts("Starting descriptions fetcher:)")
+    links = LinksToVisit.get_links()
+
+    descs =
+      Task.async_stream(links, fn link -> get_description_from_page(link.link) end)
+      |> Enum.to_list()
+
+    IO.inspect(descs)
+  end
+
+  def get_description_from_page(relative_link) do
+    url = "#{@baseUrl}#{relative_link}"
+
+    # TODO: figure out if i need multiple of this
+    req = Req.new(http_errors: :raise)
+
+    body =
+      Req.get!(req,
+        url: url
+      ).body
+
+    {:ok, document} = Floki.parse_document(body)
+
+    document |> Floki.find("div.detailed-text span") |> Floki.text()
+  end
 end
--- lib/sculptures_in_oslo/fetch_links.ex
+++ lib/sculptures_in_oslo/fetch_links.ex
@@ -4,6 +4,7 @@ defmodule SculpturesInOslo.FetchLinks do
   @moduledoc """
   Documentation for `SculpturesInOslo`.
   """
+  alias SculpturesInOslo.FetchDescriptions
   alias SculpturesInOslo.LinksToVisit
 
   def start_link(arg) do
@@ -15,26 +16,8 @@ defmodule SculpturesInOslo.FetchLinks do
     |> Stream.map(fn {:ok, links} -> LinksToVisit.add_links(links) end)
 
     IO.puts("Done fetching links!")
-  end
 
-  @doc """
-  Get description of sculpture from page
-  """
-
-  def get_description_from_page(relative_link) do
-    url = "#{@baseUrl}#{relative_link}"
-
-    # TODO: figure out if i need multiple of this
-    req = Req.new(http_errors: :raise)
-
-    body =
-      Req.get!(req,
-        url: url
-      ).body
-
-    {:ok, document} = Floki.parse_document(body)
-
-    document |> Floki.find("div.detailed-text span") |> Floki.text()
+    spawn(&FetchDescriptions.run/0)
   end
 
   @doc """
