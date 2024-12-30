fcbc8aa8dc1cf88188c0f78496094273d27b27fb
Thusan Arul <piruthusan@gmail.com>
2024-12-30 21:55:34 +0100

    Sorts statues by proximity to gronland

--- salesman/src/main.rs
+++ salesman/src/main.rs
@@ -1,29 +1,51 @@
-use std::env;
+use std::{cmp::Ordering, env};
 
-use statue::{Statue, Statues};
+use latlon::{LatLon, GRONLAND_TBANE};
+use statue::Statue;
 
 mod latlon;
 mod statue;
 
 fn main() -> eyre::Result<()> {
     let args: Vec<String> = env::args().collect();
-
     if args.len() != 2 {
         panic!("Send in path to file plis");
     }
 
     let path = args[1].clone();
 
-    let statues = get_from_path(path)?;
+    let mut statues: Vec<Statue> = get_from_path(path)?
+        .into_iter()
+        .filter(|val| val.has_pos())
+        .collect();
 
-    println!("{:?}", &statues[..3]);
+    println!("before sort:\n{:#?}", &statues[..3]);
+    let start = LatLon::new(GRONLAND_TBANE.0, GRONLAND_TBANE.1);
+
+    // Sort statues by proximity to start point for testing
+    statues.sort_by(|a: &Statue, b: &Statue| -> Ordering {
+        let a_pos: LatLon = a.try_into().unwrap();
+        let b_pos: LatLon = b.try_into().unwrap();
+
+        let diff = a_pos.calculate_distance_to(&start) - b_pos.calculate_distance_to(&start);
+
+        if diff == 0 {
+            return Ordering::Equal;
+        } else if diff < 0 {
+            return Ordering::Less;
+        } else {
+            return Ordering::Greater;
+        }
+    });
+
+    println!("after sort:\n{:#?}", &statues[..3]);
 
     Ok(())
 }
 
-fn get_from_path(path: String) -> eyre::Result<Statues> {
+fn get_from_path(path: String) -> eyre::Result<Vec<Statue>> {
     let mut rdr = csv::Reader::from_path(path)?;
-    let mut output: Statues = vec![];
+    let mut output = vec![];
 
     for record in rdr.deserialize() {
         let record: Statue = record?;
